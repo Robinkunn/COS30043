5b72b1e0c48e5c52b6d53e015006fa9707aae032
Robinkunn <chewrobin44@gmail.com>
2025-06-11 15:30:20 +0800

    a

--- functions/index.js
+++ functions/index.js
@@ -2,27 +2,13 @@ const functions = require("firebase-functions");
 const axios = require("axios");
 
 exports.proxyAPI = functions.https.onRequest(async (req, res) => {
-  // Set CORS headers for the Firebase Function response
-  res.set('Access-Control-Allow-Origin', 'https://pizzahat.web.app');
-  res.set('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
-  res.set('Access-Control-Allow-Headers', 'Content-Type');
-
-  // Handle preflight request
-  if (req.method === 'OPTIONS') {
-    res.status(204).send('');
-    return;
-  }
-
   try {
+    // Forward the request to your InfinityFree API
     const response = await axios({
       method: req.method,
       url: `https://pizzahat.wuaze.com/api_user.php${req.url}`,
       data: req.body,
       params: req.query,
-      headers: {
-        // Forward necessary headers
-        'Content-Type': req.get('Content-Type') || 'application/json'
-      }
     });
     res.status(response.status).send(response.data);
   } catch (error) {
@@ -30,3 +16,4 @@ exports.proxyAPI = functions.https.onRequest(async (req, res) => {
     res.status(500).send({error: "Proxy request failed"});
   }
 });
+
